77bf8f19182f3d5c0de354117959851b4ac81be1
AidenKitamura <kokojj123@gmail.com>
2019-11-25 22:55:19 +0800

    Bug fix

--- app/src/main/java/com/example/notificationlistener3/TimerActivity.java
+++ app/src/main/java/com/example/notificationlistener3/TimerActivity.java
@@ -3,9 +3,8 @@ package com.example.notificationlistener3;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,10 +17,12 @@ public class TimerActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        final FrameLayout root = findViewById(R.id.TimerActivity);
         setContentView(R.layout.activity_timer);
+        final FrameLayout root = findViewById(R.id.TimerActivity);
 
         DynamicBackground.initialize();
+
+        root.setBackground(DynamicBackground.getBackground());
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable(){
@@ -32,12 +33,12 @@ public class TimerActivity extends AppCompatActivity {
         }, 10000);
 
         TimerView timerView = TimerView.getInstance(this);
-        timerView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        timerView.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
         timerView.reset();
         root.addView(timerView);
 
-        ImageButton settings = findViewById(R.id.settings);
-        ImageButton toggle = findViewById(R.id.toggle);
+        Button settings = findViewById(R.id.settings);
+        Button toggle = findViewById(R.id.toggle);
 
         settings.bringToFront();
         toggle.bringToFront();
